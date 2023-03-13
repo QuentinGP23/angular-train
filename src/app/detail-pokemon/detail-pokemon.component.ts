20c0ba91757538ca1764e430f30414e8aad1c980
QuentinGP <quentgeoffpro@yahoo.com>
2023-03-13 15:12:57 +0100

    .

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit} from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute, Router} from '@angular/router';
 import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
-  templateUrl: './detail-pokemon.component.html',
-  styles: [
-  ]
+  templateUrl: './detail-pokemon.component.html'
 })
 export class DetailPokemonComponent implements OnInit{
 
   pokemonList: Pokemon[];
-  pokemon:Pokemon|undefined;
+  pokemon: Pokemon|undefined;
 
-  constructor(private router: ActivatedRoute){}
+  constructor(private route: ActivatedRoute, private router: Router){}
 
-  ngOnInit(): void {
+  ngOnInit(){
     this.pokemonList = POKEMONS;
-      const pokemonId: string|null = this.router.snapshot.paramMap.get('id');
-      if(pokemonId){
-        this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-      }else{
-        this.pokemon = undefined;
-      }
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+    console.log(pokemonId);
+    if(pokemonId){
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    }
+  }
+  goToPokemonList(){
+    this.router.navigate(['/pokemons']);
   }
 }
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,11 +1,14 @@
 import { Component} from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pokemon-list';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
 })
+
 export class ListPokemonComponent{
+  constructor(private router: Router){}
   pokemon:Pokemon[] = POKEMONS;
 }
