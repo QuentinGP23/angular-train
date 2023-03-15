107511adc5798b53ddd12be1b0ba88605632f51d
QuentinGP23 <quentgeoffpro@yahoo.com>
2023-03-15 14:28:37 +0100

    .

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   template: `
-    <p>
-      page-not-found works!
-    </p>
+    <div class='center'>
+      <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png"/>
+      <h1>Hey, cette page n'existe pas !</h1>
+      <a routerLink="/pokemons" class="waves-effect waves-teal btn-flat">
+        Retourner à l' accueil
+      </a>
+    </div>
   `,
   styles: [
   ]
